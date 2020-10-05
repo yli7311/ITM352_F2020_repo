4b8b66fb2df78b3c5248f02ec10369ddd99f10c0
yli7311 <yli7311@hawaii.edu>
2020-10-05 13:29:59 -1000

    lab 8

--- Lab8/Ex1.js
+++ Lab8/Ex1.js
@@ -1,13 +1,15 @@
 age_count = 1; // start age count
 age = 20;
 while(age_count < age) {
-    if(age_count >= age/2) {
-        // console.log(`age ${age_count}`);
+    if( (age_count >= age/2) && (age_count < (3/4)*age) ) {
+        console.log(`No age zone!`);
+        process.exit();
         console.log(`I'm old!`);
-        break;
+        //break;
     }
-    console.log(`age ${age_count}`);
+    else { 
+        console.log(`age ${age_count}`);
+    }
     age_count++;
-
 }
 console.log(`Yi is ${age_count} years old.`)
---
+++ Lab8/Ex3.js
@@ -0,0 +1,6 @@
+//start the age count 
+age = 20;
+for (var age_count = 50; age_count > age; age_count--) {
+    console.log(`age ${age_count}`);
+}
+console.log(`Yi is ${age_count} years old.`);
